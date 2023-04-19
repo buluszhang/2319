106e1b39ae7433fb5ed56b6f9f40d9b14b29386d
youxiaer <youxiaer@qq.com>
2023-04-19 17:39:44 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <hello/>
+</template>
+
+<script>
+import hello from '@/components/hello.vue'
+export default {
+    components:{
+        hello
+    }
+}
+</script>
+
+<style  lang='less' scoped>
+    
+</style>
---
+++ src/components/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>组件</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'HelloWord'
+}
+</script>
+
+<style  lang='less' scoped>
+    
+</style>
